4bc09de3b60806d9ac1c24380fc473cc3b68bfdd
Dhiego Bastos Machado <dhiego.machado@animaeducacao.com.br>
2020-04-13 18:00:56 -0300

    Versão inicial PoC

---
+++ CpfValidatorPerformance.cs
@@ -0,0 +1,250 @@
+﻿using System;
+
+namespace BenchmarkdotnetTool
+{
+    public class CpfValidatorPerformance
+    {
+        public static bool ValidarCPFClassico(string sourceCPF)
+        {
+            if (String.IsNullOrWhiteSpace(sourceCPF))
+                return false;
+
+            string clearCPF;
+            clearCPF = sourceCPF.Trim();
+            clearCPF = clearCPF.Replace("-", "");
+            clearCPF = clearCPF.Replace(".", "");
+
+            if (clearCPF.Length != 11)
+            {
+                return false;
+            }
+
+            int[] cpfArray;
+            int totalDigitoI = 0;
+            int totalDigitoII = 0;
+            int modI;
+            int modII;
+
+            if (clearCPF.Equals("00000000000") ||
+                clearCPF.Equals("11111111111") ||
+                clearCPF.Equals("22222222222") ||
+                clearCPF.Equals("33333333333") ||
+                clearCPF.Equals("44444444444") ||
+                clearCPF.Equals("55555555555") ||
+                clearCPF.Equals("66666666666") ||
+                clearCPF.Equals("77777777777") ||
+                clearCPF.Equals("88888888888") ||
+                clearCPF.Equals("99999999999"))
+            {
+                return false;
+            }
+
+            foreach (char c in clearCPF)
+            {
+                if (!char.IsNumber(c))
+                {
+                    return false;
+                }
+            }
+
+            cpfArray = new int[11];
+            for (int i = 0; i < clearCPF.Length; i++)
+            {
+                cpfArray[i] = int.Parse(clearCPF[i].ToString());
+            }
+
+            for (int posicao = 0; posicao < cpfArray.Length - 2; posicao++)
+            {
+                totalDigitoI += cpfArray[posicao] * (10 - posicao);
+                totalDigitoII += cpfArray[posicao] * (11 - posicao);
+            }
+
+            modI = totalDigitoI % 11;
+            if (modI < 2) { modI = 0; }
+            else { modI = 11 - modI; }
+
+            if (cpfArray[9] != modI)
+            {
+                return false;
+            }
+
+            totalDigitoII += modI * 2;
+
+            modII = totalDigitoII % 11;
+            if (modII < 2) { modII = 0; }
+            else { modII = 11 - modII; }
+            if (cpfArray[10] != modII)
+            {
+                return false;
+            }
+            // CPF Válido!
+            return true;
+        }
+
+
+        // ################################################
+
+
+        public struct Cpf
+        {
+            private readonly string _value;
+
+            public readonly bool EhValido;
+            public Cpf(string value)
+            {
+                _value = value;
+
+                if (value == null)
+                {
+                    EhValido = false;
+                    return;
+                }
+
+                var posicao = 0;
+                var totalDigito1 = 0;
+                var totalDigito2 = 0;
+                var dv1 = 0;
+                var dv2 = 0;
+
+                bool digitosIdenticos = true;
+                var ultimoDigito = -1;
+
+                foreach (var c in value)
+                {
+                    if (char.IsDigit(c))
+                    {
+                        var digito = c - '0';
+                        if (posicao != 0 && ultimoDigito != digito)
+                        {
+                            digitosIdenticos = false;
+                        }
+
+                        ultimoDigito = digito;
+                        if (posicao < 9)
+                        {
+                            totalDigito1 += digito * (10 - posicao);
+                            totalDigito2 += digito * (11 - posicao);
+                        }
+                        else if (posicao == 9)
+                        {
+                            dv1 = digito;
+                        }
+                        else if (posicao == 10)
+                        {
+                            dv2 = digito;
+                        }
+
+                        posicao++;
+                    }
+                }
+
+                if (posicao > 11)
+                {
+                    EhValido = false;
+                    return;
+                }
+
+                if (digitosIdenticos)
+                {
+                    EhValido = false;
+                    return;
+                }
+
+                var digito1 = totalDigito1 % 11;
+                digito1 = digito1 < 2
+                    ? 0
+                    : 11 - digito1;
+
+                if (dv1 != digito1)
+                {
+                    EhValido = false;
+                    return;
+                }
+
+                totalDigito2 += digito1 * 2;
+                var digito2 = totalDigito2 % 11;
+                digito2 = digito2 < 2
+                    ? 0
+                    : 11 - digito2;
+
+                EhValido = dv2 == digito2;
+            }
+
+            public static implicit operator Cpf(string value)
+                => new Cpf(value);
+
+            public override string ToString() => _value;
+        }
+
+
+        // ################################################
+
+
+        public static bool ValidarCPFStackalloc(string sourceCPF)
+        {
+            static bool VerificaTodosValoresSaoIguais(ref Span<int> input)
+            {
+                for (var i = 1; i < 11; i++)
+                {
+                    if (input[i] != input[0])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            if (string.IsNullOrWhiteSpace(sourceCPF))
+                return false;
+
+            Span<int> cpfArray = stackalloc int[11];
+            var count = 0;
+
+            foreach (var c in sourceCPF)
+            {
+                if (char.IsDigit(c))
+                {
+                    if (count > 10)
+                    {
+                        return false;
+                    }
+                    cpfArray[count] = c - '0';
+                    count++;
+                }
+            }
+
+            if (count != 11) return false;
+            if (VerificaTodosValoresSaoIguais(ref cpfArray)) return false;
+
+
+            var totalDigitoI = 0;
+            var totalDigitoII = 0;
+            int modI;
+            int modII;
+
+            for (var posicao = 0; posicao < cpfArray.Length - 2; posicao++)
+            {
+                totalDigitoI += cpfArray[posicao] * (10 - posicao);
+                totalDigitoII += cpfArray[posicao] * (11 - posicao);
+            }
+
+            modI = totalDigitoI % 11;
+            if (modI < 2) { modI = 0; }
+            else { modI = 11 - modI; }
+
+            if (cpfArray[9] != modI)
+            {
+                return false;
+            }
+
+            totalDigitoII += modI * 2;
+
+            modII = totalDigitoII % 11;
+            if (modII < 2) { modII = 0; }
+            else { modII = 11 - modII; }
+
+            return cpfArray[10] == modII;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkdotnetTool;
+
+namespace MyBenchmarks
+{
+    //[SimpleJob(RuntimeMoniker.Net472, baseline: true)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp31, baseline: true)]
+    //[SimpleJob(RuntimeMoniker.Mono)]
+    [RPlotExporter]
+    public class CpfValidatorBenchmark
+    {
+        public CpfValidatorBenchmark()
+        {
+
+        }
+
+        [Benchmark]
+        public void ValidacaoClassica() => CpfValidatorPerformance.ValidarCPFClassico("10474916642");
+
+        [Benchmark]
+        public void ValidacaoOtimizada() => new CpfValidatorPerformance.Cpf("10474916642").ToString();
+
+        [Benchmark]
+        public void ValidacaoStackalloc() => CpfValidatorPerformance.ValidarCPFStackalloc("10474916642");
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // var summary = BenchmarkRunner.Run<Md5VsSha256>();
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig());
+        }
+    }
+}
